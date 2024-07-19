309801f6b0a21238011a8a376907cfee51fda22b
HMatras <hubertinio.hm@gmail.com>
2024-07-19 15:28:23 +0100

    EDIT: Header.test.js file. Removed unneccesary tests and fixed neccesary tests.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import Header from './Header';
 import { BrowserRouter } from 'react-router-dom';
@@ -6,7 +5,7 @@ import { BrowserRouter } from 'react-router-dom';
 function App() {
   return (
     <BrowserRouter>
-    <Header />
+      <Header />
     </BrowserRouter>
   );
 }
