d6e843e60b7c709ef4add2112a2b64c7fc7a1809
akhil-maram <akhil.maram@gmail.com>
2024-07-19 14:22:33 +0100

    UPDATE: LoginForm.jsx elements have updated classNames for CSS UPDATE: LoginForm.css classes have been renamed to suit LoginForm.jsx

--- frontend/src/Components/LoginForm.jsx
+++ frontend/src/Components/LoginForm.jsx
@@ -4,13 +4,13 @@ import "../resources/LoginForm.css";
 
 const LoginForm = () => {
   return (
-    <div className='form-container'>
+    <div className='login-form-container'>
         <h1>Welcome to Sky Social</h1>
         <br />
         <form method='post'>
-            <div className='input-container'>
-                <input type="text" placeholder='Email or username' name='username' className='form-control input-field' id='username'/>
-                <input type="password" name='password' id='password' placeholder='Password' className='form-control input-field'/>
+            <div className='login-form-input-container'>
+                <input type="text" placeholder='Email or username' name='username' className='form-control login-form-input-field' id='username'/>
+                <input type="password" name='password' id='password' placeholder='Password' className='form-control login-form-input-field'/>
                 <input type="submit" value="Log in" name='login' className='btn btn-outline-primary form-submit-button'/>
             </div>
         </form>
