20b1719d8c4743e4dc3351b2b547bc82e8ac24fa
dilpreetshoker04 <dilpreet.singh@sky.uk>
2024-07-24 14:38:31 +0100

    EDIT: added Header and EventsRail template to app.js. Also got rid of the default react pp stuff

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,22 @@
 import './App.css';
-
+import Header from './Components/Header'
+import EventsRail from './Components/EventsRail';
+import EventTile from './Components/EventTile'
+import { BrowserRouter } from 'react-router-dom';
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header/>
+      <EventsRail>
+        <EventTile title ="Football 5-a-side" image = "e619f4e1-c156-419e-8d26-f2cc46235e76.jpg" tags = {["5-a-side","SkyFootball"]}/>
+        <EventTile title ="Tennis Doubles" image = "download.jpeg" tags = {["5-a-side","SkyFootball"]}/>
+        <EventTile title ="Running" image = "download (1).jpeg" tags = {["5-a-side","SkyFootball"]}/>
+        <EventTile title ="Football 5-a-side" image = "e619f4e1-c156-419e-8d26-f2cc46235e76.jpg" tags = {["5-a-side","SkyFootball"]}/>
+        <EventTile title ="Tennis Doubles" image = "download.jpeg" tags = {["5-a-side","SkyFootball"]}/>
+      </EventsRail>
     </div>
+    </BrowserRouter>
   );
 }
 
