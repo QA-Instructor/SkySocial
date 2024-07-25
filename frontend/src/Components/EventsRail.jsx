e0176f7031f4ff7c5780a560087d4751a98709ea
dilpreetshoker04 <dilpreet.singh@sky.uk>
2024-07-25 17:01:08 +0100

    COMPLETED: EventRail completed

--- frontend/src/Components/EventsRail.jsx
+++ frontend/src/Components/EventsRail.jsx
@@ -1,20 +1,16 @@
-import React, { useState, useRef } from 'react';
+import React, { useRef } from 'react';
 import '../resources/eventsRail.css';
 import { MdArrowForwardIos, MdArrowBackIos } from "react-icons/md";
 import { Container } from 'react-bootstrap';
 
 export const EventsRail = ({children}) => {
     let scrl = useRef(null);
-    const [scrollX, setscrollX] = useState(0);
-    const [scrollEnd, setScrollEnd] = useState(false);
-  
-    const slide = (shift) => {
-      scrl.current.scrollBy({
-        left: shift,
-        behavior: 'smooth'
-      });
+    const [scrollX, setScrollX] = React.useState(0);
+    const [scrollEnd, setScrollEnd] = React.useState(false);
+
+    function slide(shift){
       scrl.current.scrollLeft += shift;
-      setscrollX(scrollX + shift);
+      setScrollX(scrollX + shift);
       if (Math.floor(scrl.current.scrollWidth - scrl.current.scrollLeft) <= scrl.current.offsetWidth) {
         setScrollEnd(true);
       } else {
@@ -23,7 +19,7 @@ export const EventsRail = ({children}) => {
     };
   
     const checkScrollIsAtEnd = () => {
-      setscrollX(scrl.current.scrollLeft);
+      setScrollX(scrl.current.scrollLeft);
       if (Math.floor(scrl.current.scrollWidth - scrl.current.scrollLeft) <= scrl.current.offsetWidth) {
         setScrollEnd(true);
       } else {
@@ -32,7 +28,7 @@ export const EventsRail = ({children}) => {
     };
   
     return (
-      <Container fluid className='item-slider-container'>
+      <div fluid className='item-slider-container'>
         <div className='item-slider'>
           <div  onClick={() => slide(-230)} className={`left-arrow-left ${(scrollX < 1) ? 'is-disabled-hide' : ''}`}>
             <MdArrowBackIos size="70px" />
@@ -40,11 +36,11 @@ export const EventsRail = ({children}) => {
           <div ref={scrl} onScroll={checkScrollIsAtEnd} className="item-container">
             {children}
           </div>
-          <div data-testid ="right" className={`right-arrow-right ${(!scrollEnd) ? '' : 'is-disabled-hide'}`} onClick={() => slide(+230)}>
+          <div className={`right-arrow-right ${(!scrollEnd) ? '' : 'is-disabled-hide'}`} onClick={() => slide(+230)}>
             <MdArrowForwardIos size="70px" />
           </div>
         </div>
-      </Container>
+      </div>
     );
 };
 
--- frontend/src/test/EventsRail.test.js
+++ frontend/src/test/EventsRail.test.js
@@ -1,44 +1,24 @@
 import React from 'react';
 import { create } from 'react-test-renderer';
-import { screen, act as tlract, render } from '@testing-library/react'
-import { BrowserRouter } from 'react-router-dom';
-import userEvent from '@testing-library/user-event';
-
 import EventsRail from '../Components/EventsRail';
+afterEach(() => {
+    // restore the spy created with spyOn
+    jest.restoreAllMocks();
+  });
 
-// describe('Event Tile Snapshot tests',()=>{
-//     test(`EventTile snapshot test`, () => {
-//         const header = create(<EventsRail></EventsRail>);
-//         expect(header.toJSON()).toMatchSnapshot();
-//     });
-// });
-
-describe('EventsRail functionaility tests',()=>{
-    test(`Right Scroll button works`, async ()=> {
-        await tlract(async ()=> {
-            await render(<EventsRail>
-                <div width="200px" height="200px"></div>
-                <div width="200px" height="200px"></div>
-                <div width="200px" height="200px"></div>
-                <div width="200px" height="200px"></div>
-                <div width="200px" height="200px"></div>
-                <div width="200px" height="200px"></div>
-            </EventsRail>)
-        });
-        const rightButton = screen.getByTestId("right")
-        tlract(async()=> userEvent.click(rightButton));  
+describe('Event Tile Snapshot tests',()=>{
+    test(`EventTile snapshot test`, () => {
+        const testRenderer = create(<EventsRail>
+            <div width="200px" height="200px">1</div>
+            <div width="200px" height="200px">2</div>
+            <div width="200px" height="200px">3</div>
+            <div width="200px" height="200px">4</div>
+            <div width="200px" height="200px">5</div>
+            <div width="200px" height="200px">6</div>
+            <div width="200px" height="200px">7</div>
+            <div width="200px" height="200px">8</div>
+            <div width="200px" height="200px">9</div>
+        </EventsRail>);
+        expect(testRenderer.toJSON()).toMatchSnapshot();
     });
-    test(`Left Scroll button works`, async () => {
-        await tlract(async ()=> {
-            await render(<EventsRail>
-                <div width="200px" height="200px"></div>
-                <div width="200px" height="200px"></div>
-                <div width="200px" height="200px"></div>
-                <div width="200px" height="200px"></div>
-                <div width="200px" height="200px"></div>
-                <div width="200px" height="200px"></div>
-            </EventsRail>)
-        });
-         
-    });  
 });
