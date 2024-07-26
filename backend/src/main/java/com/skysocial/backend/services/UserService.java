908259bd57fb6c38e263e4b2ae721e3f7bc9aacf
akhil-maram <akhil.maram@gmail.com>
2024-07-26 14:18:25 +0100

    EDIT: Included endpoints in UserService without functionality

--- backend/src/main/java/com/skysocial/backend/services/UserService.java
+++ backend/src/main/java/com/skysocial/backend/services/UserService.java
@@ -1,12 +1,54 @@
 package com.skysocial.backend.services;
 
 
-import com.skysocial.backend.entities.UserRepo;
+import com.skysocial.backend.entities.User;
 import com.skysocial.backend.entities.UserRepo;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class UserService {
 
-    private final UserRepo
+    private final UserRepo repo;
+
+    public UserService(UserRepo repo) {
+        this.repo = repo;
+    }
+
+    public void getLoginCredentials() {
+        // Service call for getting login credentials
+    }
+
+    public User getProfileByEmail() {
+        // Service call for getting profiles from email
+        return null;
+    }
+
+    //list of events
+    public List<String> getRegisteredUserEvents() {
+        // Service call for getting events the user is registered to
+        return null;
+    }
+
+    //list of events
+    public List<String> getCreatedUserEvents() {
+        // Service call for getting events the user is registered to
+        return null;
+    }
+
+    public boolean updateUserAccount() {
+        // Service call for updating user account
+        return true;
+    }
+
+    public boolean deleteUserAccount() {
+        // Service call for deleting user account
+        return true;
+    }
+
+    public User createAccount() {
+        // Service call for creating user account
+        return null;
+    }
 }
