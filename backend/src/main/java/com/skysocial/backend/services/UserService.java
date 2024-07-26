075f445bf675c3c95f535042e853b699dbae9588
Dhyey Ghetia <dhyey.ghetia06@gmail.com>
2024-07-26 14:10:55 +0100

    CREATE: user service file

---
+++ backend/src/main/java/com/skysocial/backend/services/UserService.java
@@ -0,0 +1,12 @@
+package com.skysocial.backend.services;
+
+
+import com.skysocial.backend.entities.UserRepo;
+import com.skysocial.backend.entities.UserRepo;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    private final UserRepo
+}
