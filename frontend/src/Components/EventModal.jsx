a18ef9a8a777def4e2c822e35c9b1012d3c57b3d
Oliver Sinnassamy <osinnassamy@gmail.com>
2024-07-26 15:31:23 +0100

    FIX: Modal missing contact info

--- frontend/src/Components/EventModal.jsx
+++ frontend/src/Components/EventModal.jsx
@@ -7,15 +7,15 @@ import Button from 'react-bootstrap/Button'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../css/EventModal.css';
 
-const EventModal = (props) => {
+const EventModal = ({event}) => {
 
     const [show, setShow] = useState(false);
 
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
 
-    const date = props.event.startDateTime.split(" ")[0];
-    const time = props.event.startDateTime.split(" ")[1]
+    const date = event.startDateTime.split(" ")[0];
+    const time = event.startDateTime.split(" ")[1];
 
     return (
         <>
@@ -28,25 +28,32 @@ const EventModal = (props) => {
                     <CloseButton id="closeBtn" onClick={handleClose}/>
                 </ModalHeader>
                 <Modal.Body>
-                    <h1>{props.event.eventTitle}</h1>
+                    <h1>{event.eventTitle}</h1>
                     <div className='event-modal-time-info'>
                         <p id="eventModalDate">Date: {date} </p>
                         <p id="eventModalTime">Time: {time} </p>
-                        <p id="eventModalSpaces">Spaces Remaining:  {props.event.currentParticipants} / {props.event.maxParticipants}</p> 
+                        <p id="eventModalSpaces">Spaces Remaining:  {event.currentParticipants} / {event.maxParticipants}</p> 
                     </div>
 
-                    <img className="event-modal-image" src={props.event.image} alt='Image of event'></img>
+                    <img className="event-modal-image" src={event.image} alt='Image of event'></img>
 
                     <div className='event-modal-location'>
-                        <p>Event Info: {props.event.eventDescription}</p>
-                        <p>Address: {props.event.eventLocation}</p>
+                        <p>Event Info: {event.eventDescription}</p>
+                        <p>Address: {event.eventLocation}</p>
                     </div>
 
-                    {/* <div>
-                        <img src={props.event.organiserImage}/>
-                        <p>Tel: {props.event.organiserTel}</p>
-                        <p>Email: {props.event.organiserEmail}</p>
-                    </div> */}
+                    <div className='event-modal-contact-container'>
+                        <div className='event-modal-contact-image-container'>
+                            <img className="event-modal-contact-image" src={event.organiser.profileImage} alt='Profile Picture'/>
+                        </div>
+                        
+                        <div event-modal-contact-container>
+                            <p>{event.organiser.firstName} {event.organiser.lastName}</p>
+                            <p>Tel: {event.organiser.email}</p>
+                            <p>Email: {event.organiser.phone}</p>
+                        </div>
+                       
+                    </div>
                 </Modal.Body>
                 <Modal.Footer>
                     <Button variant="primary" onClick={handleClose}>
