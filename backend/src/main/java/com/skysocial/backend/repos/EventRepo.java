2c64114884bcc2260994122b1a5f411b344c1949
akhil-maram <akhil.maram@gmail.com>
2024-07-28 15:59:59 +0100

    EDIT: Added comment to specify where interface methods go

--- backend/src/main/java/com/skysocial/backend/repos/EventRepo.java
+++ backend/src/main/java/com/skysocial/backend/repos/EventRepo.java
@@ -4,4 +4,5 @@ import com.skysocial.backend.entities.event.Event;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface EventRepo extends JpaRepository<Event,Long> {
+    // Interface methods go here
 }
