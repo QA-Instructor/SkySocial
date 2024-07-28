75b5012c583fd4dd5bdb61d398127258d27087bd
akhil-maram <akhil.maram@gmail.com>
2024-07-28 16:22:47 +0100

    EDIT: EventController contains functionality for getting all events and getting an event on its ID

--- backend/src/main/java/com/skysocial/backend/EventController.java
+++ backend/src/main/java/com/skysocial/backend/EventController.java
@@ -1,17 +1,34 @@
 package com.skysocial.backend;
 
+import com.skysocial.backend.entities.event.Event;
+import com.skysocial.backend.repos.EventRepo;
+import jakarta.websocket.server.PathParam;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class EventController {
 
     // private EventService service;
+    private EventRepo repo;
 
     @GetMapping("/eventcontrollerhealth")
     public String health() {
         return "Event Controller is up and running";
     }
 
-    
+    @GetMapping("/getAllEvents")
+    public List<Event> getAllEvents() {
+        // return this.service.getAllEvents();
+        return null;
+    }
+
+    @GetMapping("/")
+    public Event getEventById(@PathParam("id") Long id) {
+        // return this.service.getEventById(id);
+        return null;
+    }
+
 }
