0be7b27d80dd8b3bc6e3a9ea3620b7b78d8c633a
dilpreetshoker04 <dilpreet.singh@sky.uk>
2024-07-30 12:24:56 +0100

    EDIT: added navigation tests to app.test.js

--- frontend/src/test/App.test.js
+++ frontend/src/test/App.test.js
@@ -1,6 +1,6 @@
 import { create } from 'react-test-renderer';
 import App from '../App';
-import { getByText } from '@testing-library/react';
+import { render, screen, fireEvent} from '@testing-library/react';
 
 describe('Snapshot tests',()=>{
     test(`App renders`, () => {
@@ -8,4 +8,31 @@ describe('Snapshot tests',()=>{
         expect(app.toJSON()).toMatchSnapshot();
     });
 });
+
+describe('Navigation tests',()=>{
+  test('Profile Page is naviagted',()=>{
+      render( <App/>)
+      const profileButton = screen.getByText(/Profile/i);
+      expect(profileButton).toBeInTheDocument();
+      fireEvent.click(profileButton);
+      const profileText = screen.getByText(/Profile Page Goes here/i)
+      expect(profileText).toBeInTheDocument();
+  })
+  test('Create Event is naviagted',()=>{
+    render( <App/>)
+    const createEventButton = screen.getByText(/Create Event/i);
+    expect(createEventButton).toBeInTheDocument();
+    fireEvent.click(createEventButton);
+    const createEventText = screen.getByText(/Create Event Page Goes here/i)
+    expect(createEventText).toBeInTheDocument();
+  })
+  test('My Events is naviagted',()=>{
+    render( <App/>)
+    const myEventsButton = screen.getByText(/My Events/i);
+    expect(myEventsButton).toBeInTheDocument();
+    fireEvent.click(myEventsButton);
+    const myEventsText = screen.getByText(/My Events Page Goes here/i)
+    expect(myEventsText).toBeInTheDocument();
+  })
+})
 
