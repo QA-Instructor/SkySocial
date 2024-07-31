b7a3815925a8faa94692985f0dd074cbed784dd8
dilpreetshoker04 <dilpreet.singh@sky.uk>
2024-07-31 16:31:18 +0100

    FIX: fixed images

--- frontend/src/Components/Banner.jsx
+++ frontend/src/Components/Banner.jsx
@@ -2,25 +2,28 @@ import React from 'react';
 // import '../resources/banner.css';
 import Carousel from 'react-bootstrap/Carousel';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import '../resources/banner.css'
 function Banner() {
   return (
     <Carousel>
       <Carousel.Item>
         <img
-          className='d-block w-100'
+          className='carousel-img'
           src="https://www.bscglasgow.co.uk/images/2023/womens5/11.jpg"
-          alt="First Slide"
+          alt="Second Slide"
+          height="200"
         />
         <Carousel.Caption>
           <h3>Football</h3>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
+        
         <img
-          className='d-block w-100'
+          className='carousel-img'
           src="https://www.luxurytravelmag.com.au/wp-content/uploads/2022/11/AudleyPublicHouse_Interior_Sim-Canetty-Clarke.jpg"
           alt="Second Slide"
+          height="200"
         />
         <Carousel.Caption>
          <h3>Pub Social</h3>
@@ -28,9 +31,10 @@ function Banner() {
       </Carousel.Item>
       <Carousel.Item>
         <img
-          className='d-block w-100'
+          className='carousel-img'
           src="https://upload.wikimedia.org/wikipedia/commons/e/e4/Latte_and_dark_coffee.jpg"
           alt="Thrid Slide"
+          height="200"
         />
         <Carousel.Caption>
           <h3>Coffee chats with local coffee beans toaster</h3>
