6aa2f67eff4f1973a7a6cc48a4a8e7aeee6286b8
akhil-maram <akhil.maram@gmail.com>
2024-08-01 14:42:39 +0100

    EDIT: Linked Registration Form to backend API call

--- frontend/src/Components/RegistrationForm.jsx
+++ frontend/src/Components/RegistrationForm.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import FloatingLabel from 'react-bootstrap/FloatingLabel';
 import Form from 'react-bootstrap/Form';
 import { useNavigate } from 'react-router-dom';
@@ -10,24 +10,30 @@ const URL = "http://localhost:8080"
 const RegistrationForm = () => {
     const navigate = useNavigate();
 
-    const registerData = {
+    const [isRegisterClicked, setIsRegisterClicked] = useState(false);
+    const [registerData, setRegisterData] = useState({
         firstName : "",
         lastName : "",
         email : "" ,
-        phone : "",
+        phone : null,
         profileImage : ""
-    }
+    });
 
     const onFormChange = (key, value) => {
-        registerData[key] = value;
-    }
+        setRegisterData(prevFormData => ({ ...prevFormData, [key]: value, }))
+    };
   
-    const onFormSubmit = () => {
-      console.log(registerData)
-      useEffect(() => {
-        axios.post(URL + "/createAccount", registerData).then(navigate('/Login')).catch(alert("Your account has not been created"))
-      ,[]})
+    const onFormSubmit = (e) => {
+        e.preventDefault()
+      setIsRegisterClicked(true)
+      setRegisterData(registerData)
     }
+
+    useEffect(() => {
+        if (isRegisterClicked) {
+            axios.post(URL + "/createAccount", registerData).then(navigate("/Login")).catch(err => alert(err))
+        }
+    }, [isRegisterClicked])
   
   return (
     <div className='registration-form-container'>
@@ -35,7 +41,7 @@ const RegistrationForm = () => {
             <FloatingLabel controlId="floatingInput" label="Upload an image" className="mb-3">
                 <Form.Control type="file" placeholder="Upload an profile photo" name="profileImage" accept="image/*" onChange={e => onFormChange(e.target.name, e.target.value)}/>
             </FloatingLabel>
-        <form className='registration-form-input-container'>
+        <form className='registration-form-input-container' method='post' onSubmit={e => onFormSubmit(e)}>
             <FloatingLabel
                 controlId="floatingInput"
                 label="Email Address"
@@ -57,6 +63,13 @@ const RegistrationForm = () => {
             >
                   <Form.Control type="text" placeholder="Last Name" name='lastName' onChange={e => onFormChange(e.target.name, e.target.value)} required/>
             </FloatingLabel>
+            <FloatingLabel
+                controlId="floatingInput"
+                label="Phone Number"
+                className="mb-3"
+            >
+                  <Form.Control type="text" placeholder="Phone Number" name='phone' onChange={e => onFormChange(e.target.name, e.target.value)} required/>
+            </FloatingLabel>
               <FloatingLabel
                   controlId="floatingPassword"
                   label="Password"
@@ -69,7 +82,7 @@ const RegistrationForm = () => {
                   className="mb-3">
                   <Form.Control type="password" placeholder="Confirm Password" name='confirmPassword' required/>
               </FloatingLabel>
-              <button type="submit" className='btn btn-outline-primary form-submit-button' name="create" onClick={onFormSubmit}>Create Account</button>
+              <button type="submit" className='btn btn-outline-primary form-submit-button' name="create">Create Account</button>
         </form>
         <p>If you already have an account:</p>
         <button type="submit" className='btn btn-outline-success' name="login" onClick={()=>navigate("/Login")}>Login</button>
