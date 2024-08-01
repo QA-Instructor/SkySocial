cc5ccea0017e6f96d6d5477f640028c1f1e9ed0c
Oliver Sinnassamy <osinnassamy@gmail.com>
2024-08-01 11:23:11 +0100

    CREATE: add form submission to register, login and profile

--- frontend/src/Components/LoginForm.jsx
+++ frontend/src/Components/LoginForm.jsx
@@ -3,16 +3,34 @@ import "../resources/LoginForm.css";
 import "bootstrap/dist/css/bootstrap.min.css"
 import { useNavigate } from 'react-router-dom';
 const LoginForm = () => {
+
+  const loginData = {
+    "username" : "",
+    "userPassword" : ""
+  }
+
+
+  const onFormChange = (key, value) => {
+      loginData[key] = value;
+  }
+
+  const onFormSubmit = () => {
+    console.log(loginData)
+    //Make axios request
+    navigate('/')
+  }
+
+
   const navigate = useNavigate();
   return (
     <div className='login-form-container'>
         <h1>Welcome to Sky Social</h1>
         <br />
-        <form method='post'>
+        <form>
             <div className='login-form-input-container'>
-                <input type="text" placeholder='Email or username' name='username' className='form-control login-form-input-field' id='username'/>
-                <input type="password" name='password' id='password' placeholder='Password' className='form-control login-form-input-field'/>
-                <input type="submit" value="Log in" name='login' className='btn btn-outline-primary form-submit-button' onClick={()=>navigate("/")}/>
+                <input type="text" placeholder='Email or username' name='username' className='form-control login-form-input-field' id='username' onChange={e => onFormChange(e.target.name, e.target.value)}/>
+                <input type="password" name='userPassword' id='password' placeholder='Password' className='form-control login-form-input-field' onChange={e => onFormChange(e.target.name, e.target.value)}/>
+                <input type="submit" value="Log in" name='login' className='btn btn-outline-primary form-submit-button' onClick={onFormSubmit}/>
             </div>
         </form>
         <br />
--- frontend/src/Components/ProfileForm.jsx
+++ frontend/src/Components/ProfileForm.jsx
@@ -5,13 +5,26 @@ import { FaPencilAlt } from "react-icons/fa";
 
 const ProfileForm = ({user}) => {
   const defaultImage = "https://uxwing.com/wp-content/themes/uxwing/download/peoples-avatars/person-profile-image-icon.png";
-
   const [isDisabled, setEditable] = useState(true);
-
   const handleEditable = () => setEditable(!isDisabled);
 
-  //Add function to save changes to profile.
-  const saveProfile = () => {};
+  const profileData = {
+    firstName : user.firstName,
+    lastName : user.lastName,
+    email : user.email ,
+    phone : user.phone,
+    profileImage : user.profileImage
+  }
+
+  const onFormChange = (key, value) => {
+    console.log("change: " + key)
+    profileData[key] = value;
+  } 
+
+  const onFormSubmit = () => {
+    console.log(profileData)
+    //Make axios request
+  }
 
   return (
     <div className='profile-form-container'>
@@ -19,21 +32,21 @@ const ProfileForm = ({user}) => {
         <form className='profile-form-form-container'>
         <div class="profile-form-btn-container">
             <a id="editBtn" className='profile-form-edit-button profile-form-save-text' onClick={handleEditable}>
-                {!isDisabled ? <span onClick={saveProfile}>Save</span> : <span id="editBtnText"><FaPencilAlt/></span>}
+                {!isDisabled ? <span onClick={onFormSubmit}>Save</span> : <span id="editBtnText"><FaPencilAlt/></span>}
             </a>
         </div>
         <div>
           {/* Integrate PictureUpload component */}
-          { user.image === "" ? <img className="profile-form-image" src={defaultImage} alt="Profile"/> : <img className="profile-form-image" src={user.image} alt="Profile"/> }
+          { user.profileImage === "" ? <img className="profile-form-image" src={defaultImage} alt="Profile"/> : <img className="profile-form-image" src={user.profileImage} alt="Profile"/> }
         </div>
 
             <div className='profile-form-input-container'>
-              <input type='text' className="form-control profile-form-input-field" name="firstname" disabled={isDisabled} value={user.firstname} />
-              <input type='text' className="form-control profile-form-input-field" name="lastname" disabled={isDisabled} value={user.lastname} />
+              <input type='text' className="form-control profile-form-input-field" name="firstName" disabled={isDisabled} defaultValue={user.firstName} onChange={e => onFormChange(e.target.name, e.target.value)} />
+              <input type='text' className="form-control profile-form-input-field" name="lastName" disabled={isDisabled} defaultValue={user.lastName} onChange={e => onFormChange(e.target.name, e.target.value)} />
             </div>           
             <div className='profile-form-input-container'>
-              <input type='email' className="form-control profile-form-input-field" name="email" disabled={isDisabled} value={user.email} />
-              <input type='tel' className="form-control profile-form-input-field" name="phone" disabled={isDisabled} value={user.phone} />
+              <input type='email' className="form-control profile-form-input-field" name="email" disabled={isDisabled} defaultValue={user.email} onChange={e => onFormChange(e.target.name, e.target.value)} />
+              <input type='tel' className="form-control profile-form-input-field" name="phone" disabled={isDisabled} defaultValue={user.phone} onChange={e => onFormChange(e.target.name, e.target.value)} />
             </div>
             <div className='profile-control-button-container'>
               <button id="deleteBtn" type="button" className='btn btn-danger profile-form-control-btn'>Delete Account</button>
